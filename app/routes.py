ad3fd2f7e0f7f1a7a0dbc2e07e59d103fe5fa8f7
Connor Milliken <cmilliken09@gmail.com>
2018-09-10 14:01:09 -0400

    Added navbar functionality and a few other pages

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template, url_for, redirect
 
 @app.route('/')
 @app.route('/index')
@@ -8,3 +8,18 @@ def index():
     num = 3
     names = ['Connor', 'Chet', 'Todd', 'Tyler', 'Fiona', 'Tara']
     return render_template('index.html', name=name, num=num, names=names)
+
+@app.route('/data')
+def data_func():
+    person = {
+        'name': 'Max',
+        'weight': 185,
+        'height': 72,
+        'eyes': 'Blue',
+        'active': True
+    }
+    return render_template('data.html', person=person)
+
+@app.route('/redirecting')
+def my_redirect():
+    return redirect(url_for('index'))
